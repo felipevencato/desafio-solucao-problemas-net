4fb766f9b6fb84ed1d0fd2ccadc19b5333a42579
Felipe Vencato <felipevencato@gmail.com>
2021-04-15 21:07:00 -0300

    Adicionado arquivo com pPonto de inicialização do projeto e arquivo de interface que todos os exercícios devem seguir

---
+++ Run.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace DIO
+{
+    class Run
+    {
+        static void Main(string[] args)
+        {
+            var Exe = new List<IRun>{
+            };
+            if (args.Count() == 0)
+            {
+                int i = 0;
+                Console.WriteLine(
+                    "Informe o numero do Executorício\n"
+                    + string.Join("\n", Exe.Select(x => $"{i++} {x}"))
+                    + "\ndotnet run [NUMERO]"
+                );
+                return;
+            }
+	    int Op = Int32.Parse(args[0]);
+	    if (Op > Exe.Count)
+		    Console.WriteLine("Opção Inválida");
+            else
+		    Exe.ElementAt((Op == 0)? 0:Op - 1).Run();
+        }
+    }
+}
